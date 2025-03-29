f4ab3661d85e5f2d040c48fcd189f32d050c014c
NagiFish <94735845+nezikog@users.noreply.github.com>
2025-03-29 13:21:18 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     debugShowCheckedModeBanner: false,
+      
       home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -34,168 +35,33 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
    
     return Scaffold(
-      backgroundColor: Color.fromRGBO(12, 58, 29, 1),
       appBar: AppBar(
-        backgroundColor: Color.fromRGBO(12, 58, 29, 1),
-      ),
-      body: Center(
-       
-        child: Column(
-          
-          children: <Widget>[
-    Column(
-              children: [
-                
-                Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-  'Создайте аккаунт',
-  textAlign: TextAlign.start,
-  style: TextStyle(
-        fontFamily: 'Inter Tight',
-        letterSpacing: 0.0,
-        fontSize: 30,
-        color: Colors.white
-      ),
-)
-              ],
-            ),
-            Padding(
-  padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 16),
-  child: Container(
-    width: double.infinity,
-    child: TextFormField(
-      autofocus: true,
-      autofillHints: [AutofillHints.email],
-      obscureText: false,
-      decoration: InputDecoration(
-        labelText: 'Email',
-        labelStyle: TextStyle(
-              fontFamily: 'Inter',
-              letterSpacing: 0.0,
-
-            ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            width: 5,
-                        color: Color.fromRGBO(33, 211, 161, 1),
-          ),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            width: 5,
-                        color: Color.fromRGBO(33, 211, 161, 1),
-          ),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            width: 5,
-                        color: Color.fromRGBO(33, 211, 161, 1),
-          ),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        focusedErrorBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            width: 5,
-            color: Color.fromRGBO(33, 211, 161, 1),
-          ),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        filled: true,
-        fillColor: Colors.white,
         
-      ),
-      style: TextStyle(
-            fontFamily: 'Inter',
-            letterSpacing: 0.0,
-          ),
-      keyboardType: TextInputType.emailAddress,
-    ),
-  ),
-),
-Padding(
-  padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 16),
-  child: Container(
-    width: double.infinity,
-    child: TextFormField(
-      autofocus: true,
-      autofillHints: [AutofillHints.password],
-      decoration: InputDecoration(
-        labelText: 'Password',
-        labelStyle: TextStyle (
-              fontFamily: 'Inter',
-              letterSpacing: 0.0,
-                      
-            ),
-        enabledBorder: OutlineInputBorder(
-          
-          borderSide: BorderSide(
-            width: 5,
-                        color: Color.fromRGBO(33, 211, 161, 1)
-          ),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            width: 5,
-                        color: Color.fromRGBO(33, 211, 161, 1)
-          ),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        errorBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            width: 5,
-            color: Color.fromRGBO(33, 211, 161, 1)
-          ),
-          borderRadius: BorderRadius.circular(12),
-        ),
-        focusedErrorBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            width: 5,
-                        color: Color.fromRGBO(33, 211, 161, 1)
-          ),
-          borderRadius: BorderRadius.circular(12),
-          
-        ),
-        filled: true,
+        backgroundColor: Color.fromRGBO(24, 120, 83, 1),
+        title: Text("HackPack",
+        style: GoogleFonts.getFont('JetBrains Mono'),
         
-        contentPadding: EdgeInsets.all(24),
-        suffixIcon: InkWell(
-          onTap: () => (
-          ),
-          focusNode: FocusNode(skipTraversal: true),
-          child: Icon(
-
-                 Icons.visibility_outlined,
-            size: 24,
-            color: Colors.white,
-          ),
         ),
-                fillColor: Colors.white,
+        actions: [
+          Container(
+            width: 60,
+            height: 100,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black)
+            ),
+            child: IconButton(onPressed: (){}, icon: Icon(Icons.auto_awesome)),
+          )
+        ],
       ),
-      style: TextStyle(
-            fontFamily: 'Inter',
-            letterSpacing: 0.0,
-          ),
-    ),
-  ),
-),
-Padding(padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
-      child: ElevatedButton(onPressed: (){ print("Clicked!!!");}, child: Text("Создать", style: TextStyle(fontSize: 22)),
-      style: ButtonStyle(
-        backgroundColor: Color.fromRGBO(35, 211, 162, 1),
-      ),
-      ),
-)
-              ],
-            ), 
-         
-           
-          ]
+      body: ListView(
+       children: [
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
         )
+       ],
+        
       )
     );
   }
