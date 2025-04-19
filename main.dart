1fea3d5336098441731825ef99ef690c47b7d2af
NagiFish <94735845+nezikog@users.noreply.github.com>
2025-04-19 13:18:46 +0300

    main.dart

--- main.dart
+++ main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hackpack/reg.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp1());
 }
 
 class MyApp extends StatelessWidget {
